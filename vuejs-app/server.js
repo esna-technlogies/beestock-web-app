3ed5af850e4dd76c0260e6d0c15995954d7b5f8a
Ahmed Shendy <dev.ahmed.shendy@gmail.com>
2018-04-01 11:40:21 +0200

    Updated config.yml

---
+++ vuejs-app/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const serveStatic = require('serve-static');
+
+const history = require('connect-history-api-fallback');
+
+let app = express();
+app.use(serveStatic(__dirname + "/dist"));
+
+app.use(history({
+  disableDotRule: true
+}));
+
+app.use(serveStatic(__dirname + "/dist"));
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log('Listening on port ' + port)
+});
